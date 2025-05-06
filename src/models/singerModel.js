ebf3965efd5ca7b0379f0f3b3fc8304046907a90
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-05-06 11:12:26 -0300

    refactor: adjust singer model

--- src/models/singerModel.js
+++ src/models/singerModel.js
@@ -1,57 +1,35 @@
 const pool = require("../config/database");
 
 const getSingers = async () => {
-    const result = await pool.query(
-        `   SELECT singers.*, musics.name AS music_name
-            FROM singers
-            LEFT JOIN musics ON singers.music_id = musics.id
-        `
-    );
+    const result = await pool.query("SELECT * FROM singers");
     return result.rows;
 };
 
 const getSingerById = async (id) => {
+    const result = await pool.query("SELECT * FROM singers WHERE id = $1", [id]);
+    return result.rows[0];
+};
+
+const createSinger = async (name, age, genre) => {
     const result = await pool.query(
-        `   SELECT singers.*, musics.name AS music_name
-            FROM singers
-            LEFT JOIN musics ON singers.music_id = musics.id
-            WHERE singers.id = $1
-        `, [id]
+        "INSERT INTO singers (name, age, genre) VALUES ($1, $2, $3) RETURNING *",
+        [name, age, genre]
     );
     return result.rows[0];
 };
 
-const createSinger = async (name, age, music_id) => {
+const updateSinger = async (id, name, age, genre) => {
     const result = await pool.query(
-        `   INSERT INTO singers (name, age, music_id)
-            VALUES ($1, $2)
-            RETURNING *
-        `, [name, age, music_id]	
-    );
-    return result.rows[0];
-};
-
-const updateSinger = async (id, name, age, music_id) => {
-    const result = await pool.query(
-        `   UPDATE singers
-            SET name = $1,
-            age = $2,
-            music_id = $3
-            WHERE id = $4
-            RETURNING *
-        `, [name, age, music_id, id]
+        "UPDATE singers SET name = $1, age = $2, genre = $3 WHERE id = $4 RETURNING *",
+        [name, age, genre, id]
     );
     return result.rows[0];
 };
 
 const deleteSinger = async (id) => {
-    const result = await pool.query(
-        `   DELETE FROM singers
-            WHERE id = $1
-            RETURNING *
-        `, [id]
-    );
-    return { message: `Cantor de id ${id} deletado com sucesso.`};
-};
+    const result = await pool.query("DELETE FROM singers WHERE id = $1 RETURNING *", [id]);
+
+    return { message: `Cantor de id ${id} deletado com sucesso.` };
+}
 
 module.exports = { getSingers, getSingerById, createSinger, updateSinger, deleteSinger };
